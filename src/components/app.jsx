269cc7e48b045c718aea05d185e3277814afdff2
Sara Scaini <sa.scaini@gmail.com>
2019-05-25 15:03:49 +0100

    created App and SearchBar components

---
+++ src/components/app.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import SearchBar from './search_bar.jsx';
+
+class App extends Component {
+
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+        </div>
+        <div className="right-scene"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
